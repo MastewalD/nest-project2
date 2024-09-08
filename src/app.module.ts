67c5771462ca2018bd758220c2ad942fd079245e
mastewal <mastewaldegu0@gmail.com>
2024-09-07 22:13:02 -0300

    integrate nest with postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-
+import { TypeOrmModule } from "@nestjs/typeorm"
+import * as dotenv from "dotenv"
+dotenv.config()
 @Module({
-  imports: [UserModule],
+  imports: [UserModule,TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT, // Convert to number
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
